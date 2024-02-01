3d4e37400fc13c50a1ca4729788384824efcefcd
SrwaFarkha <111921225+SrwaFarkha@users.noreply.github.com>
2024-02-01 15:24:41 +0100

    commit

--- Models/Dto/AccountDto.cs
+++ Models/Dto/AccountDto.cs
@@ -8,7 +8,6 @@
         public string EmailAddress { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
-
         public string City { get; set; }
         public string StreetAddress { get; set; }
 
